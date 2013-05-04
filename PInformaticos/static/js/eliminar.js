619f2fb4e2e0c9fad0d3bc76148e29fd9dccc5d0
Francisco Padilla <francisco.padilla@catolica.edu.sv>
2013-05-03 19:28:51 -0600

    Agregando plantillas por segunda ves

---
+++ PInformaticos/static/js/eliminar.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+			$('#datetimepicker').datetimepicker({
+				format: 'dd/MM/yyyy',
+				language: 'es-SV',
+				pickTime: false
+			});
+			
+			$('#datetimepicker3').datetimepicker({
+				pickDate: false,
+				pickSeconds: false,
+				pick12HourFormat: true
+			});
+			
+			$('#datetimepicker4').datetimepicker({
+				pickDate: false,
+				pickSeconds: false,
+				pick12HourFormat: true
+			});
+
+		</script>
